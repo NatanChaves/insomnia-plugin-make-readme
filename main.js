3942c2ed9d5ead985c3fdf1d374efa3b7c2483b9
NatanChaves <nataninacio1chaves@gmail.com>
2024-02-27 15:41:39 -0300

    validacao de campos obrigatorios

--- main.js
+++ main.js
@@ -1,38 +1,66 @@
 module.exports.requestActions = [
   {
-    
     label: "Generate Read.me",
-    action: async (context, data) => {
+    action: async (contextxRequest, data) => {
       const { request } = data;
+      const parameters = request.parameters;
+      const requiredParameters = [];
 
-      const response = await context.network.sendRequest(request);
+      //Validar obrigatoriedade de cada parametro
+      for (let i = 0; i < request.parameters.length; i++) {
+        module.exports.requestHooks = [
+          (context) => {
+            //remove parametro para testar se é obrigatório
+            context.request.removeParameter(parameters[i].name);
+          },
+        ];
+
+        //envia request
+        const response = await contextxRequest.network.sendRequest(request);
+
+        //valida se é obrigatório de acordo com statuscode do response
+        if (response.statusCode > 399 && response.statusCode < 500) {
+          requiredParameters.push(parameters[i].name);
+        }
+      };
+
+      //regex para extrair path do endpoint:
       const regex = /https?:\/\/[^\/]+(\/[^?#]*)/;
       const match = request.url.match(regex);
       const endpoint = match && match[1];
 
-      var parametros = `<p>| Nome | Valor | Descrição | Obrigatório? |</p>`;
+      //define parametros
+      var parametros = `| Nome | Valor | Descrição | Obrigatório? | <br> | ------ | ------ | ------ | ------ | <br>`;
+      parameters.forEach((item) => {
+        var obrigatorio = 'Não';
 
-      request.parameters.forEach((item) => {
-        parametros += `<p>| ${item.name} | ${item.value} | Sua descrição do campo | S ou N |</p>`;
+        if (requiredParameters.includes(item.name)) {
+          obrigatorio = 'Sim'
+        }
+        parametros += `| ${item.name} | ${item.value} | Sua descrição do campo | ${obrigatorio} | <br>`;
       });
 
-      var headers = `<p>| Header | Valor |</p>`;
-
+      //define headers
+      var headers = `| Header | Valor | <br> | ------ | ------ | <br>`;
       request.headers.forEach((item) => {
-        headers += `<p>| ${item.name} | ${item.value} |</p>`;
+        headers += `| ${item.name} | ${item.value} | <br>`;
       });
 
       const html = `
-      <p>### Descricao do endpoint detalhando quais dados são retornados ou operações que ele realiza </p> <br> 
-      <p>## Url completa: <br>${request.url}.</p> <br>
-      <p>## Endpoint: <br>${endpoint}.</p> <br>
-      <p>## Method: <br>${request.method}.</p> <br>
-      <p>## Url: <br>${request.url}.</p><br>
-      <p>## Headers <br> ${headers}<br> </p><br>
-      <p>## Filtros</p> <br>${parametros}<br> <p></p>
-      <p>## Examples</p> <br>\`\`\`<embed type="text/plain" width="600" height="200" src="${response.bodyPath}"></embed> \`\`\`<br> <p></p>
+        ### Descricao do endpoint detalhando quais dados são retornados ou operações que ele realiza  <br><br>
+        ## Url completa: <br> ${request.url}. <br><br>
+        ## Endpoint: <br> ${endpoint}. <br><br>
+        ## Method: <br> ${request.method}. <br><br>
+        ## Headers: <br> ${headers}  <br>
+        ## Filtros: <br> ${parametros}  <br><br>
+        ## Examples: <br> \`\`\` \`\`\`  <br>
       `;
-      context.app.showGenericModalDialog(`# ${request.name}`, { html });
+
+      const code = document.createElement("code");
+      code.innerHTML = html;
+      code.style.userSelect = "all";
+
+      contextxRequest.app.dialog(`Readme`, code);
     },
   },
 ];
